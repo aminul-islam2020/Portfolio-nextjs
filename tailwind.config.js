406f06804c02b831a7e7045585ee8195f29fca6b
Aminul Islam <aminulislam2020@gmail.com>
2025-07-15 01:59:39 +0600

    Header Component Added

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const { Outfit, Ovo } = require("next/font/google");
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001f'
+      },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"]
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
