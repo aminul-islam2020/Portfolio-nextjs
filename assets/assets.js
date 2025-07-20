53fa3a52ee9de9d4a4f09cdf92de5ce0fca2ffb1
Aminul Islam <aminulislam2020@gmail.com>
2025-07-21 02:14:31 +0600

    Home page updated

--- assets/assets.js
+++ assets/assets.js
@@ -36,6 +36,7 @@ import send_icon from './send-icon.png';
 import right_arrow_bold from './right-arrow-bold.png';
 import right_arrow_bold_dark from './right-arrow-bold-dark.png';
 import nameLogo from './namelogo.png';
+import linkedinimg from './linkedinimg.png';
 
 export const assets = {
     user_image,
@@ -75,7 +76,8 @@ export const assets = {
     send_icon,
     right_arrow_bold,
     right_arrow_bold_dark,
-    nameLogo
+    nameLogo,
+    linkedinimg
 };
 
 export const workData = [
--- components/Header.jsx
+++ components/Header.jsx
@@ -4,20 +4,22 @@ import Image from 'next/image'
 
 const Header = () => {
     return (
-        <div>
-            <div>
-                <Image src={assets.profile_img} alt='' className='rounded-full w-32' />
+        <>
+            <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
+                <div>
+                    <Image src={assets.linkedinimg} alt='' className='rounded-full w-32' />
+                </div>
+                <h3 className='flex items-end gap-2 text-xl md:text2xl mb-3 font-ovo'>Hi! I'm Aminul Islam <Image src={assets.hand_icon} alt='' className=' w-10' /></h3>
+                <h1 className='text-3xl sm:text-6xl lg:text-[66px font-ovo]'>Frontend web developer based in Bangladesh</h1>
+                <p className='max-2-2xl mx-auto font-ovo'>
+                    I am a frontend developner from Dhaka, Bangladesh with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.
+                </p>
+                <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+                    <a href="#contact" className='px-10 py-3 border rounded-full border-white flex bg-black text-white items-center gap-2'>Contact me <Image src={assets.right_arrow_white} alt='' className=' w-4' /></a>
+                    <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>My Resume<Image src={assets.download_icon} alt='' className=' w-4' /></a>
+                </div>
             </div>
-            <h3 className='flex items-end gap-2 text-xl md:text2xl mb-3 font-ovo'>Hi! I'm Aminul Islam <Image src={assets.hand_icon} alt='' className=' w-32' /></h3>
-            <h1 className='text-3xl sm:text-6xl lg:text-[66px font-ovo]'>Frontend web developer based in Bangladesh</h1>
-            <p className='max-2-2xl mx-auto font-ovo'>
-                I am a frontend developner from Dhaka, Bangladesh with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.
-            </p>
-            <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-                <a href="#contact" className='px-10 py-3 border rounded-full border-white flex bg-black text-white items-center gap-2'>Contact me <Image src={assets.right_arrow_white} alt='' className=' w-4' /></a>
-                <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>My Resume<Image src={assets.download_icon} alt='' className=' w-4' /></a>
-            </div>
-        </div>
+        </>
     )
 }
 
